a749020c24344276f0a700c8fe286f0fb1987388
X3R074 <guenoleducrot@gmail.com>
2025-01-23 01:28:13 +0100

    <ajout de la barre de recherche, du readme et connexion au JS>

--- script.js
+++ script.js
@@ -1,8 +1,30 @@
 const apikey = 'faef8cac' //ceci est ma clé API
 //https://www.omdbapi.com/?apikey=faef8cac&s=
-
-let results=("https://www.omdbapi.com/?apikey=",apikey,"&s=superman");
+let babidou
+let url=(`https://www.omdbapi.com/?apikey=${apikey}&s=${search}`);
+console.log (url)
 //requette pour récupérer requette pour chercher 
-results.parse = 
-console.log("bonjour");
-console.log(results);
+fetch(url)
+
+.then(response => response.json()) 
+.then (data => json=data)
+.catch(error => console.error('Erreur:', error))
+
+const info = JSON.parse(babidou);
+
+console.log(info.search[0].Title);
+console.log(info.Title);
+
+
+
+
+
+
+function Display (movies) {
+
+    const moviesContainer = document.getElementsByClassName('movies');
+  
+    
+    }
+
+//console.log(info.info);
--- search/script.js
+++ search/script.js
@@ -1,3 +1,20 @@
-const apikey = faef8cac
+const apikey = "faef8cac"
 
-let api = `https:/omdbapi.com/` 
+console.log("bonjour");
+let film = document.getElementById("search").value
+
+getData ();
+async function getData() {
+    try{
+        
+        console.log(film)
+        const reponse = await fetch(`https://omdbapi.com/?apikey=${apikey}&s=${film}`);
+        const data = await reponse.json()
+        console.log(data.Search(0).Title)
+      
+    } catch (error) {
+        console.error("Erreur:", error)
+    }
+    
+    
+}
